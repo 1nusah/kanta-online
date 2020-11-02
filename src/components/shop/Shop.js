0b14c6b548dbc2665e7015439eded8ce36273fbd
Inusah-Said <saidinusah29@gmail.com>
2020-11-02 21:41:45 +0000

    added the shop component

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid } from '@material-ui/core';
-
+import axios from 'axios';
+import Product from '../product/Product';
+import Loading from '../loading/loading';
 const Shop = () => {
+	const [shop, setshop] = useState([]);
+	console.log('i am ', typeof shop);
+	console.log(shop);
+	useEffect(() => {
+		var options = {
+			method: 'GET',
+			url: 'https://rapidapi.p.rapidapi.com/products/v2/list',
+			params: {
+				offset: '0',
+				categoryId: '4209',
+				limit: '48',
+				store: 'US',
+				country: 'US',
+				currency: 'USD',
+				sort: 'freshness',
+				lang: 'en-US',
+				q: 'shirt,women',
+				sizeSchema: 'US',
+			},
+			headers: {
+				'x-rapidapi-host': 'asos2.p.rapidapi.com',
+				'x-rapidapi-key': '583c0bbb31msh25aee6af24c8df9p1f6b0ajsn4488c2bd87b7',
+			},
+		};
+
+		axios
+			.request(options)
+			.then(function (response) {
+				setshop(response.data);
+				console.log(typeof shop);
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+	}, []);
+
 	return (
-		<Grid>
-			<h1>Hello Motherfucker</h1>
-			<h1>Hello Motherfucker</h1>
-			<h1>Hello Motherfucker</h1>
-			<h1>Hello Motherfucker</h1>
-			<h1>Hello Motherfucker</h1>
-			<h1>Hello Motherfucker</h1>
+		<Grid container className="checkout_container">
+			<Grid container>
+				{shop.length === 0 ? (
+					<Loading />
+				) : (
+					shop.products.map((item) => (
+						<Product
+							id={item.id}
+							name={item.name}
+							image={item.imageUrl}
+							price={item.price.current.text}
+						/>
+					))
+				)}
+			</Grid>
 		</Grid>
 	);
 };
