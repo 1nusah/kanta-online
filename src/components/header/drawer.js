8188b8554c03b93922993ad505188a5fc5614531
Inusah Said <saidinusah29@gmail.com>
2021-09-11 00:16:42 +0000

    updates

--- src/components/header/drawer.js
+++ src/components/header/drawer.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import clsx from 'clsx';
+
 import { Drawer, Button, List, ListItem, Grid } from '@material-ui/core';
 import './Header.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 export default function TemporaryDrawer({ open, ToggleDrawer }) {
 	const routes = ['men', 'bags', 'shoes', 'women', 'children'];
+	const history = useHistory();
+	const handleRouting = () => {
+		ToggleDrawer();
+	};
 	return (
 		<Grid container>
 			<Grid item xs={12} sm={12} lg={12} md={12}>
@@ -13,17 +18,22 @@ export default function TemporaryDrawer({ open, ToggleDrawer }) {
 					open={open}
 					variant="temporary"
 					onClose={ToggleDrawer}
-					style={{ width: '100%' }}
+					style={{ width: '100%', height: '100%' }}
 				>
 					<List
-						style={{ width: '100%', backgroundColor: '#000', color: '#adadad' }}
+						style={{
+							width: '100%',
+							backgroundColor: '#000',
+							color: '#adadad',
+							height: '100%',
+						}}
 					>
 						{routes.map((i) => (
-							//  {
-							// console.log(i);
 							<ListItem>
 								<Link to={i} style={{ textDecoration: 'none' }}>
-									<Button color="secondary">{i}</Button>
+									<Button onClick={() => handleRouting()} color="secondary">
+										{i}
+									</Button>
 								</Link>
 							</ListItem>
 						))}
